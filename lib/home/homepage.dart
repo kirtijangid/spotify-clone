d5b909d67f538687050035dcb84a6ccdd520c4bf
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-22 16:41:05 +0530

    adding text to the body container

--- lib/home/homepage.dart
+++ lib/home/homepage.dart
@@ -1,14 +1,31 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
- class HomePage extends StatelessWidget {
-   const HomePage({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Homepage'),
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(255, 29, 28, 28),
+             Color.fromARGB(255, 77, 76, 76)],
+          ),
+        ),
+        child: ListView(children: [
+          Row(
+            children: [
+              Text('Good Evening',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),)
+            ],
+          )
+        ]),
       ),
-     );
-   }
- } 
+    );
+  }
+}
