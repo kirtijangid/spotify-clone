83a45998136fbd0cecb2fafaa976c26046d756ac
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-24 17:03:33 +0530

    creating horizontal scrollable listview

--- lib/data .dart
+++ lib/data .dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/shape/gf_avatar_shape.dart';
+
 class Data {
   var playList = [
     {'name': 'Liked Songs', 'image': 'assets/images/liked.jpg', 'audio': ' '},
@@ -8,11 +11,74 @@ class Data {
 
   var recentlyPlayed = [
     {
-      'shape': '',
-      'alignment': '',
-      'name': 'Liked Songs',
-      'image': 'assets/images/liked.jpg',
-      'audio': ' '
-     },
+      'name': 'Gentle Guitar',
+      'image': 'assets/images/gentleguitar.png',
+      'shape': GFAvatarShape.square,
+      'alignment': CrossAxisAlignment.start,
+      'audio': 'https://www.computerhope.com/jargon/m/example.mp3'
+    },
+    {
+      'name': 'Alicia Keys',
+      'image': 'assets/images/aliciakeys.png',
+      'shape': GFAvatarShape.circle,
+      'alignment': CrossAxisAlignment.center,
+      'audio': 'https://www.computerhope.com/jargon/m/example.mp3'
+    },
+    {
+      'name': 'Fallin\'',
+      'image': 'assets/images/fallin.png',
+      'shape': GFAvatarShape.square,
+      'alignment': CrossAxisAlignment.start,
+      'audio':
+          'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/2e/e2/7d/2ee27d35-5e1e-0fd0-42ea-359b5256403e/mzaf_9335390342361255150.plus.aac.p.m4a'
+    },
+    {
+      'name': 'Pesa Nasha Pyar',
+      'image': 'assets/images/bohemia.png',
+      'shape': GFAvatarShape.square,
+      'alignment': CrossAxisAlignment.start,
+      'audio':
+          'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/2e/e2/7d/2ee27d35-5e1e-0fd0-42ea-359b5256403e/mzaf_9335390342361255150.plus.aac.p.m4a'
+    },
+    {
+      'name': 'Where the Light',
+      'image': 'assets/images/johnmayer.png',
+      'shape': GFAvatarShape.square,
+      'alignment': CrossAxisAlignment.start,
+      'audio':
+          'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/2e/e2/7d/2ee27d35-5e1e-0fd0-42ea-359b5256403e/mzaf_9335390342361255150.plus.aac.p.m4a'
+    },
+    {
+      'name': 'Girlfriend',
+      'image': 'assets/images/girlfriend.png',
+      'shape': GFAvatarShape.square,
+      'alignment': CrossAxisAlignment.start,
+      'audio':
+          'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/2e/e2/7d/2ee27d35-5e1e-0fd0-42ea-359b5256403e/mzaf_9335390342361255150.plus.aac.p.m4a'
+    },
+    {
+      'name': 'Thunderstruck',
+      'image': 'assets/images/thunderstruck.png',
+      'shape': GFAvatarShape.square,
+      'alignment': CrossAxisAlignment.start,
+      'audio':
+          'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/2e/e2/7d/2ee27d35-5e1e-0fd0-42ea-359b5256403e/mzaf_9335390342361255150.plus.aac.p.m4a'
+    },
+    {
+      'name': 'Hummingbird',
+      'image': 'assets/images/hummingbird.png',
+      'shape': GFAvatarShape.square,
+      'alignment': CrossAxisAlignment.start,
+      'audio':
+          'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/2e/e2/7d/2ee27d35-5e1e-0fd0-42ea-359b5256403e/mzaf_9335390342361255150.plus.aac.p.m4a'
+    },
+    {
+      'name': 'Continuum',
+      'image': 'assets/images/continuum.png',
+      'shape': GFAvatarShape.square,
+      'alignment': CrossAxisAlignment.start,
+      'audio':
+          'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/2e/e2/7d/2ee27d35-5e1e-0fd0-42ea-359b5256403e/mzaf_9335390342361255150.plus.aac.p.m4a'
+    },
   ];
 }
--- lib/home/widgets/recentlyPlaylist.dart
+++ lib/home/widgets/recentlyPlaylist.dart
@@ -1,22 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:spotify_clone/data%20.dart';
 
 class RecentlyPlayed extends StatelessWidget {
   const RecentlyPlayed({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          child: Text('Recently Played',
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Container(
+        child: const Text(
+          'Recently Played',
           style: TextStyle(
             fontSize: 25,
             fontWeight: FontWeight.bold,
+            fontFamily: 'LibreFranklin',
             color: Colors.white,
-          ),),
-        )
-      ],
-    );
+          ),
+        ),
+      ),
+     
+      Container(
+        height: 200,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: Data().recentlyPlayed.length,
+            itemBuilder: (context, index) {
+              return Container(
+                width: 150,
+                padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+                child: Column(
+                  crossAxisAlignment: Data().recentlyPlayed[index]['alignment']
+                      as CrossAxisAlignment,
+                  children: [
+                    InkWell(
+                      child: Container(
+                        child: GFAvatar(
+                          shape: Data().recentlyPlayed[index]['shape']
+                              as GFAvatarShape,
+                          backgroundImage: AssetImage(
+                              Data().recentlyPlayed[index]['image'].toString(),
+                              ),
+                              radius: 70,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    Text(
+                      Data().recentlyPlayed[index]['name'].toString(),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ],
+                ),
+              );
+            }),
+      ),
+    ]);
   }
 }
