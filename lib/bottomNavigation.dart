dfc39f504cf8e1d659ff03ae5d4c10f7e393b8bc
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-24 20:00:06 +0530

    creating navigation bar

--- lib/bottomNavigation.dart
+++ lib/bottomNavigation.dart
@@ -1,2 +1,36 @@
 import 'package:flutter/material.dart';
 
+class BottomNavigation extends StatelessWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: Colors.amber,
+      ), child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.transparent,
+          Colors.transparent
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter
+          ),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          onTap: (value) {},
+          selectedItemColor: Colors.amber,
+          unselectedItemColor: Colors.white,
+          showSelectedLabels: true,
+          items: const[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 30,),
+              label: 'Home',
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
