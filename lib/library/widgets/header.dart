e1a24538f4c7b2dcfacfb724879757ae7cff67df
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-30 00:09:34 +0530

    formating

--- lib/library/widgets/header.dart
+++ lib/library/widgets/header.dart
@@ -6,41 +6,43 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 30, left: 10, right: 10),
+      margin: const EdgeInsets.only(top: 30, left: 10, right: 10),
       child: const Row(
         children: [
           CircleAvatar(
             radius: 18,
             backgroundColor: Color.fromARGB(255, 42, 56, 252),
-            child: Text('K',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold
-            ),
+            child: Text(
+              'K',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
           SizedBox(width: 10),
-          Text('Your Library',
-          style: TextStyle(
-            fontSize: 23,
-            fontWeight: FontWeight.bold,
-            color: Colors.white
-          ),
+          Text(
+            'Your Library',
+            style: TextStyle(
+                fontSize: 23, fontWeight: FontWeight.bold, color: Colors.white),
           ),
           Row(
             children: [
-              SizedBox(width: 120,),
-              Icon(Icons.search,
-              color: Colors.white,
-              size: 28,
+              SizedBox(
+                width: 120,
               ),
-              SizedBox(width: 20,),
-              Icon(Icons.add,
-              color: Colors.white,
-              size: 28,)
-              
+              Icon(
+                Icons.search,
+                color: Colors.white,
+                size: 28,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 28,
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
