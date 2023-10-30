53f7423bd1b2aad1ab350dd6d6a2297eec9e0035
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-30 12:22:32 +0530

    adding title & subtitle

--- lib/library/library.dart
+++ lib/library/library.dart
@@ -5,7 +5,6 @@ import 'package:spotify_clone/data%20.dart';
 import 'package:spotify_clone/library/widgets/header.dart';
 import 'package:spotify_clone/library/widgets/roundedCards.dart';
 
-
 class Library extends StatelessWidget {
   const Library({super.key});
 
@@ -14,58 +13,88 @@ class Library extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black54,
       body: ListView(
-        children:  [
+        children: [
           Header(),
           const Row(
-            children:[
-          SizedBox(width: 10,),
-          RoundedCards(text: 'Playlists'),
-          SizedBox(width: 10,),
-          RoundedCards(text: 'Podcasts & shows'),
-          SizedBox(width: 10,),
-          RoundedCards(text: 'Artists'),
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              RoundedCards(text: 'Playlists'),
+              SizedBox(
+                width: 10,
+              ),
+              RoundedCards(text: 'Podcasts & shows'),
+              SizedBox(
+                width: 10,
+              ),
+              RoundedCards(text: 'Artists'),
             ],
           ),
-          SizedBox(height: 25,),
+          SizedBox(
+            height: 25,
+          ),
           const Padding(
             padding: EdgeInsets.all(5),
-          child: Row(
-            children: [
-              Row(
-                children: [
-                  SizedBox(width: 10,),
-                  Icon(Icons.compare_arrows_rounded,
-                  color: Colors.white,
-                  size: 25,
-                  ),
-                  SizedBox(width: 10,),
-                  Text('Recently Played',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 13,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  ),
-                ],
-              ),
-              SizedBox(width: 200,),
-              Icon(Icons.add_box_outlined,
-              color: Colors.white,
-              size: 18,),
-            ],
-          ),
-          ),
-          ...Data().library.map((val){
-            return  GFListTile(
-              avatar: GFAvatar(
-                backgroundImage: AssetImage(
-                  val['image'].toString()
+            child: Row(
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(
+                      Icons.compare_arrows_rounded,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Recently Played',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 13,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
-                radius: 30,
-                shape: val['shape'] as dynamic,
-              ),
-            );
-          },
+                SizedBox(
+                  width: 200,
+                ),
+                Icon(
+                  Icons.add_box_outlined,
+                  color: Colors.white,
+                  size: 18,
+                ),
+              ],
+            ),
+          ),
+          ...Data().library.map(
+            (val) {
+              return GFListTile(
+                avatar: GFAvatar(
+                  backgroundImage: AssetImage(val['image'].toString()),
+                  radius: 30,
+                  shape: val['shape'] as dynamic,
+                ),
+                title: Text(
+                  val['name'].toString(),
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 17),
+                ),
+                subTitle: Text(
+                  val['name'].toString(),
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 211, 208, 208),
+                      fontSize: 14),
+                ),
+              );
+            },
           ),
         ],
       ),
