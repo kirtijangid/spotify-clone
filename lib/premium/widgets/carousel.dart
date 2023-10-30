80e27d8f41951a59ee9a7acb64c4438b8bec957d
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-30 23:20:11 +0530

    creating a container

--- lib/premium/widgets/carousel.dart
+++ lib/premium/widgets/carousel.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:spotify_clone/data%20.dart';
 
 class Cards extends StatelessWidget {
   const Cards({super.key});
@@ -11,7 +11,29 @@ class Cards extends StatelessWidget {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: [
-          SizedBox(width:  MediaQuery.of(context).size.width / (5.7),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / (5.7),
+          ),
+          ...Data().carousel.map(
+            (e) {
+              return Container(
+                child: Row(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 140,
+                      decoration: const BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(5),
+                          topRight: Radius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
         ],
       ),
