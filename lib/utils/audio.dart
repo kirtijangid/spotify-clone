11e5dd75220b1585ee49c8ef6a8dcfe9dcd953df
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-31 23:52:24 +0530

    creating class(statefulWidget with some variables)

---
+++ lib/utils/audio.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AudioPlayerPro extends StatefulWidget {
+   AudioPlayerPro({required this.audioURL, required this.image, required this.name image, required this.name});
+
+  final String audioURL;
+  final String image;
+  final String name;
+
+  @override
+  State<AudioPlayerPro> createState() => _AudioPlayerProState();
+}
+
+class _AudioPlayerProState extends State<AudioPlayerPro> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
