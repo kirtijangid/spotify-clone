a6c8411eea38c05c168d30ed87615c5f02510317
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-31 22:21:17 +0530

    adding text in row & col

--- lib/premium/widgets/premiumPlan.dart
+++ lib/premium/widgets/premiumPlan.dart
@@ -14,12 +14,35 @@ class PremiumPlan extends StatelessWidget {
                 'Premium Individual',
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 22,
+                  fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        '₹549',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                            'FOR 9 MONTHS',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 187, 186, 186),
+                            ),
+                          ),
+                ],
+              ),
             ],
           ),
+          SizedBox(height: 30,),
         ],
       ),
     );
