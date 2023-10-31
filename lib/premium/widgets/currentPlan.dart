ab3df073c46fa802ba966b60cd736953d933a753
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-31 21:12:55 +0530

    adding padding

--- lib/premium/widgets/currentPlan.dart
+++ lib/premium/widgets/currentPlan.dart
@@ -5,36 +5,40 @@ class CurrentPlans extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 25,
-      ),
-      height: 80,
-      decoration: BoxDecoration(color: const Color.fromARGB(255, 77, 76, 75),
-      borderRadius: BorderRadius.circular(8),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            'Spotify Free',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 25,
+        ),
+        height: 80,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 77, 76, 75),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Spotify Free',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
             ),
-          ),
-           Text(
-            'CURRENT PLAN ',
-            style: TextStyle(
-              color: Color.fromARGB(255, 196, 193, 193),
-              fontWeight: FontWeight.bold,
-              fontSize: 10,
-              letterSpacing: 2,
+            Text(
+              'CURRENT PLAN ',
+              style: TextStyle(
+                color: Color.fromARGB(255, 196, 193, 193),
+                fontWeight: FontWeight.bold,
+                fontSize: 10,
+                letterSpacing: 2,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
