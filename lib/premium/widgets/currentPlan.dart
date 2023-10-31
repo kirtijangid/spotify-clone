b9a15683422bbe3a3e619d7d7265c7db4d50f040
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-10-31 20:38:57 +0530

    adding another text

--- lib/premium/widgets/currentPlan.dart
+++ lib/premium/widgets/currentPlan.dart
@@ -7,6 +7,7 @@ class CurrentPlans extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
             'Spotify Free',
@@ -14,6 +15,15 @@ class CurrentPlans extends StatelessWidget {
               color: Colors.white,
               fontWeight: FontWeight.bold,
               fontSize: 20,
+            ),
+          ),
+           Text(
+            'CURRENT PLAN ',
+            style: TextStyle(
+              color: Color.fromARGB(255, 196, 193, 193),
+              fontWeight: FontWeight.bold,
+              fontSize: 10,
+              letterSpacing: 2,
             ),
           ),
         ],
