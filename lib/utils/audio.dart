d9adfc5b16bd041216c7e0a210d9330d461243e2
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-11-01 20:26:02 +0530

    adding UI overlay style

--- lib/utils/audio.dart
+++ lib/utils/audio.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:line_icons/line_icons.dart';
 
 class AudioPlayerPro extends StatefulWidget {
-  AudioPlayerPro({required this.audioURL, required this.image, required this.name});
-   
+  AudioPlayerPro(
+      {required this.audioURL, required this.image, required this.name});
+
   final String audioURL;
   final String image;
   final String name;
-
 
   @override
   State<AudioPlayerPro> createState() => _AudioPlayerProState();
@@ -16,35 +17,38 @@ class AudioPlayerPro extends StatefulWidget {
 class _AudioPlayerProState extends State<AudioPlayerPro> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
+    return Scaffold(
       backgroundColor: const Color.fromARGB(136, 53, 52, 52),
       body: Container(
         padding: const EdgeInsets.only(left: 10, right: 10, top: 40),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.brown,
-              Colors.black87
-            ]
-          )),
-      child: const Center(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.brown, Colors.black87])),
+        child: const Center(
+          child: Column(
             children: [
-              Icon(LineIcons.angleDown,
-              color: Colors.white,
-              size: 25,)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    LineIcons.angleDown,
+                    color: Colors.white,
+                    size: 25,
+                  )
+                ],
+              )
             ],
-            
-          )
-        ],
+          ),
+        ),
       ),
-      ),
-      ),
-      );
+    );
   }
 }
