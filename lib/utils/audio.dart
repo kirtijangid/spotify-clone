321b379dcb179b6e52807e6dc853df2fb23abbd8
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-11-01 19:46:24 +0530

    adding icon and padding

--- lib/utils/audio.dart
+++ lib/utils/audio.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 
 class AudioPlayerPro extends StatefulWidget {
   AudioPlayerPro({required this.audioURL, required this.image, required this.name});
@@ -15,6 +16,26 @@ class AudioPlayerPro extends StatefulWidget {
 class _AudioPlayerProState extends State<AudioPlayerPro> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return  Scaffold(
+      backgroundColor: Color.fromARGB(136, 53, 52, 52),
+      body: Container(
+        padding: EdgeInsets.only(left: 10, right: 10, top: 40),
+      child: Center(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(LineIcons.angleDown,
+              color: Colors.white,
+              size: 25,)
+            ],
+            
+          )
+        ],
+      ),
+      ),
+      ),
+      );
   }
 }
